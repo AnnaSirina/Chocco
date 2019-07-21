1b6f1e74b87200318806d27bf6a37ab89b53e7c7
AnnaSirina <sirina.anya@gmail.com>
2019-07-21 15:11:35 +0300

    3-я страница

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,31 @@
-const { src, dest, task, series } = require("gulp");
-const rm = require( 'gulp-rm' );
+const {
+  src,
+  dest,
+  task,
+  series
+} = require("gulp");
+const rm = require('gulp-rm');
 const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
 
 sass.compiler = require('node-sass');
 
-task( "clean", () => {
-    return src( "dist/**/*", { read: false }).pipe( rm() )
-  })
+task("clean", () => {
+  return src("dist/**/*", {
+    read: false
+  }).pipe(rm())
+})
 
-  task("copy:html", () => {
-    return src('src/*.html').pipe(dest('dist'))
+task("copy:html", () => {
+  return src('src/*.html').pipe(dest('dist'))
 });
 
 
 task("styles", () => {
-    return src("src/styles/main.scss")
+  return src("src/styles/main.scss")
     .pipe(sassGlob())
     .pipe(sass().on("error", sass.logError))
     .pipe(dest("dist"));
 });
 
-task ("default", series("clean","copy:html", "styles"))
+task("default", series("clean", "copy:html", "styles"))
