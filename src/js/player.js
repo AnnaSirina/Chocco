5e860d57353d29ff21e224a89a3e5deeefb4b154
AnnaSirina <sirina.anya@gmail.com>
2019-09-01 20:58:14 +0300

    работающий плеер со своими кнопками

--- src/js/player.js
+++ src/js/player.js
@@ -4,7 +4,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player("yt-player", {
     width: "660",
     height: "405",
-    videoId: "WmLMOMwlmtI",
+    videoId: "nKq7Yy6vFTY",
     playerVars: {
       controls: 0,
       disablekb: 0,
@@ -20,64 +20,83 @@ function onYouTubeIframeAPIReady() {
   });
 }
 
-function onPlayerReady() {
-  const durationInSeconds = player.getDuration();
+function onPlayerReady(event) {
+  const duration = player.getDuration();
   let interval;
+  updateTimerDisplay();
+
+  $(".player").removeClass("hidden");
+
   clearInterval(interval);
-  interval = (setInterval(() => {
-      const completedInSeconds = player.getCurrentTime();
-      $('.player__duration-completed').text(formatTime(completedInSeconds));
-    }, 1000);
 
-    $(".player__duration-estimate").text(formatTime(durationInSeconds));
+  interval = setInterval(() => {
+    const completed = player.getCurrentTime();
+    const percents = (completed / duration) * 100;
+
+    changeButtonPosition(percents);
+
+    updateTimerDisplay();
+  }, 1000);
+}
+
+
+
+function onPlayerStateChange(event) {
+  const playerButton = $(".player__start");
+  switch (event.data) {
+    case 1:
+      $(".player__wrapper").addClass("active");
+      playerButton.addClass("paused");
+      break;
+    case 2:
+      playerButton.removeClass("paused");
+      break;
   }
+}
 
-  function formatTime(time) {
-    const roundTime = Math.round(time);
-    const minutes = Math.floor(roundTime / 60);
-    const seconds = roundTime - minutes * 60;
-    const fornattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+$(".player__start").on("click", e => {
+  const playerStatus = player.getPlayerState(); // 0 - ended, 1 - played, 2 - paused ...
 
-    return `${minutes}:${fornattedSeconds}`;
-  }
-
-  $('.player__start').on('click', e => {
-    const btn = $(e.currentTarget);
-
-    if (btn.hasClass("paused")) {
-      player.pauseVideo();
-      btn.removeClass("paused")
-    } else {
-      player.playVideo();
-      btn.addClass("paused");
-    }
-
-  })
-
-  function onPlayerStateChange(event) {
-    const playerButton = $(".player__start");
-    switch (event.data) {
-      case 1:
-        $(".player__wrapper").addClass("active");
-        playerButton.addClass("paused");
-        break;
-      case 2:
-        playerButton.removeClass("paused");
-        break;
-    }
-  }
-
-  $(".player__splash").on("click", e => {
+  if (playerStatus !== 1) {
     player.playVideo();
+  } else {
+    player.pauseVideo();
+  }
+});
+
+
+$(".player__playback").on("click", e => {
+  e.preventDefault();
+  const bar = $(e.currentTarget);
+  const newButtonPosition = e.pageX - bar.offset().left;
+  const clickedPercents = (newButtonPosition / bar.width()) * 100;
+  const newPlayerTime = (player.getDuration() / 100) * clickedPercents;
+
+  changeButtonPosition(clickedPercents);
+  player.seekTo(newPlayerTime);
+});
+
+$(".player__splash").on("click", e => {
+  player.playVideo();
+});
+
+function changeButtonPosition(percents) {
+  $(".player__playback-button").css({
+    left: `${percents}%`
   });
+}
 
-  function changeButtonPosition(percents) {
-    $(".player__playback-button").css({
-      left: `${percents}%`
-    });
-  }
+function updateTimerDisplay() {
+  $(".player__duration-completed").text(formatTime(player.getCurrentTime()));
+  $(".player__duration-estimate").text(formatTime(player.getDuration()));
+}
 
-  function updateTimerDisplay() {
-    $(".player__duration-completed").text(formatTime(player.getCurrentTime()));
-    $(".player__duration-estimate").text(formatTime(player.getDuration()));
-  }
+function formatTime(time) {
+  const roundTime = Math.round(time);
+
+  const minutes = Math.floor(roundTime / 60);
+  const seconds = roundTime - minutes * 60;
+  const formatedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return minutes + ":" + formatedSeconds;
+}
